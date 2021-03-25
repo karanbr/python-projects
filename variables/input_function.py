8ea1d1b3dd2f0ea0c7a7eb2e98f5f28ade6f34dc
karan <karan.bruellau1997@gmail.com>
2021-03-25 22:55:02 +0100

    basics with variables

---
+++ variables/input_function.py
@@ -0,0 +1,6 @@
+name = input("Whats your name?: ")
+print("Hello " + name)
+print("hello " + input("Whats your name?") + "!")
+
+name = input("Whats your name? ")
+print(len(name))
---
+++ variables/variable_excercise.py
@@ -0,0 +1,20 @@
+# Challane
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+# Switch a and b values
+####################################
+# Write your code below this line 👇
+a_value = a
+b_value = b
+
+b = a_value
+a = b_value
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
