345d8570c0f598c4a9b551e8ab8297363b7d13d4
karan <karan.bruellau1997@gmail.com>
2021-05-13 13:37:09 +0200

    added nato alphabet app

--- comprehension/pandas/main.py
+++ comprehension/pandas/main.py
@@ -6,19 +6,29 @@ student_dict = {
 }
 
 student_dataframe = pandas.DataFrame(student_dict)
+print("**********************")
+print("DATAFRAME:")
 print(student_dataframe)
+print("**********************")
 
+print("**********************")
+print("ALL VALUES")
 for (key, value) in student_dataframe.items():
     print(value)  # shows all values and not a single one like we want
+print("**********************")
 
+print("**********************")
+print("INBUILT LOOP")
 # instead use inbuilt loop called iterrows -> Loop through rows of df
 for (index, row) in student_dataframe.iterrows():
     # print(row)
     print(row.student)
+print("**********************")
 
 for (index, row) in student_dataframe.iterrows():
     print(row.score)
 
+print("**********************")
 for (index, row) in student_dataframe.iterrows():
     if row.student == "Angela":
         print(row.score)
